4e704ec0dcbf1cde49b2d956353e094e7e7cea34
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2023-12-13 00:21:13 +0000

    separate header

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import { useCallback } from 'react'
 import { useLocalStorage } from 'react-use'
 
 import styles from './App.module.css'
-import githubDark from "./assets/github-mark.svg"
-import githubLight from "./assets/github-mark-white.svg"
-
-const servers = new Map<string, string>([
-  ["jp", "Japanese"],
-  ["en", "English/Global"],
-  ["tw", "Taiwan"],
-  ["kr", "Korean"],
-])
+import Header from './Header'
 
 function App() {
   const lightMode = matchMedia("(prefers-color-scheme: light)").matches;
-  const githubLogo = <img className={styles.headerLogo} src={lightMode ?  githubDark : githubLight} />
 
-  const [server, setServer] = useLocalStorage("server", "en", {raw: true})
-  if (typeof server != "string" || !servers.has(server)) {setServer("en")}
+  const [server = "en", setServer] = useLocalStorage<string>("server", "en", {raw: true})
 
-  const [showPastUpdates, setShowPastUpdates] = useLocalStorage("showPastUpdates", "false", {raw: true})
-  console.log(showPastUpdates, typeof showPastUpdates)
-
-  const serverOptions: React.ReactElement[] = []; 
-  servers.forEach((value, key) => {
-    serverOptions.push(<option key={key} onClick={useCallback(() => {setServer(key)}, [setServer])} value={key}>{value}</option>)
-  });
+  const [showPastUpdates = false, setShowPastUpdates] = useLocalStorage("showPastUpdates", false, {raw: false, deserializer: Boolean, serializer: String})
 
   return (
     <>
-      <div className={ styles.header }>
-        <div className={`${styles.item} ${styles.valign}`}>Project Sekai Updates</div>
-        <div className={`${styles.item} ${styles.middle}`}></div>
-        <div className={`${styles.item} ${styles.valign}`}>
-          <input onChange={useCallback(() => {setShowPastUpdates(showPastUpdates == "true" ? "false" : "true")}, [showPastUpdates, setShowPastUpdates])} type="checkbox" id="showPastUpdatesBox" checked={showPastUpdates == "true"} />
-          <label className={ styles.noselect } htmlFor="showPastUpdatesBox">Show Past Updates</label>
-        </div>
-        <div className={ styles.item }>
-          <label className={ styles.noselect } htmlFor="serverSelect">Server:</label>
-          <br />
-          <select id="serverSelect" defaultValue={server}>{serverOptions}</select>
-        </div>
-        <a className={ styles.item } href="https://github.com/JdavisBro/SekaiUpdates" target="_blank">
-          {githubLogo}
-        </a>
-      </div>
+      <Header
+        lightMode={lightMode}
+        setServer={setServer} server={server}
+        setShowPastUpdates={setShowPastUpdates} showPastUpdates={showPastUpdates}
+      />
       <div className={ styles.content }>
         HELLO!
       </div>
