a562a8c4085bc4c7bdcb9b419c5c81c5429c6cee
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2024-01-03 04:42:51 +0000

    Change updates to a glob import

--- src/updates/updates.ts
+++ src/updates/updates.ts
@@ -1,22 +1,11 @@
 import { UpdateType } from "../types/UpdateType.ts";
-import v2_4_0 from "./v2_4_0.ts";
-import v2_5_0 from "./v2_5_0.ts";
-import v2_6_0 from "./v2_6_0.ts";
-import v2_7_0 from "./v2_7_0.ts";
-import v2_8_0 from "./v2_8_0.ts";
-import v3_0_0 from "./v3_0_0.ts";
-import v3_1_0 from "./v3_1_0.ts";
-import v3_2_0 from "./v3_2_0.ts";
 
-const updates: UpdateType[] = [
-  v2_4_0,
-  v2_5_0,
-  v2_6_0,
-  v2_7_0,
-  v2_8_0,
-  v3_0_0,
-  v3_1_0,
-  v3_2_0,
-];
+const modules = import.meta.glob<{ default: UpdateType }>("./v*.ts", {
+  eager: true,
+});
+
+const updates: UpdateType[] = Object.values(modules).map(
+  (value) => value.default,
+);
 
 export default updates;
