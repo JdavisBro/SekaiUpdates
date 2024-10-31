bb3f25f522fbad017f289192f6caed5cfa858a1b
Your Name <44114870+JdavisBro@users.noreply.github.com>
2024-10-31 00:18:07 +0000

    update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useLocalStorage } from "react-use";
-
 import styles from "./App.module.css";
-import Header from "./Header";
 import Content from "./Content/Content";
 import { Server } from "./types/ServerType";
 import BigFeature from "./Modal/BigFeature";
@@ -13,34 +10,13 @@ function App() {
       "[DEV] Project Sekai Updates";
   }
 
-  const [server = Server.en, setServer] = useLocalStorage<Server>(
-    "server",
-    Server.en,
-    { raw: false, deserializer: Number, serializer: String },
-  );
-
-  const [showPastUpdates = false, setShowPastUpdates] = useLocalStorage(
-    "showPastUpdates",
-    false,
-    {
-      raw: false,
-      deserializer: (value) => value == "true",
-      serializer: String,
-    },
-  );
+  const server = Server.en
 
   return (
     <>
       <div className={styles.container}>
-        <Header
-          setServer={setServer}
-          server={server}
-          setShowPastUpdates={setShowPastUpdates}
-          showPastUpdates={showPastUpdates}
-        />
         <Content
           server={server}
-          showPastUpdates={showPastUpdates}
           setDisplayFeature={useCallback((value) => {
             window.location.hash = value;
           }, [])}
--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -1,14 +1,17 @@
 import { useRef, useState } from "react";
+import { useLocalStorage, useMedia } from "react-use";
 
 import styles from "./Content.module.css";
 import updates from "../updates/updates";
 import Update from "./Update";
 import { Server } from "../types/ServerType";
-import { useLocalStorage } from "react-use";
+
+import githubDark from "../assets/github-mark.svg";
+import githubLight from "../assets/github-mark-white.svg";
+import info from "../assets/info.svg";
 
 type Props = {
   server: Server;
-  showPastUpdates: boolean;
   setDisplayFeature: (newHash: string) => void;
 };
 
@@ -16,6 +19,25 @@ export default function Content(props: Props): React.ReactElement {
   const [search, setSearch] = useState("");
   const [searchDescription, setSearchDescription] = useState(false);
   const [displayGrid, setDisplayGrid] = useLocalStorage("displayGrid", true);
+  const [showPastUpdates = false, setShowPastUpdates] = useLocalStorage(
+    "showPastUpdates",
+    false,
+    {
+      raw: false,
+      deserializer: (value) => value == "true",
+      serializer: String,
+    },
+  );
+
+  const lightMode = useMedia("(prefers-color-scheme: light)");
+
+  const githubLogo = (
+    <img
+      className={styles.headerLogo}
+      alt="GitHub Logo"
+      src={lightMode ? githubDark : githubLight}
+    />
+  );
 
   const searchRef = useRef<HTMLInputElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,47 +45,84 @@ export default function Content(props: Props): React.ReactElement {
   return (
     <>
       <div className={styles.content}>
-        <div className={styles.optionscontainer}>
-          <span className={styles.searchcontainer}>
-            <label htmlFor="searchInput">Filter: </label>
-            <input
-              type="text"
-              id="searchInput"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyUp={(e) => {
-                if ((e.key == "Enter" || e.key == " ") && searchRef.current) {
-                  searchRef.current.blur();
-                }
-              }}
-              ref={searchRef}
-            />
-          </span>
-          <span className={styles.checkboxcontainer}>
-            <input
-              type="checkbox"
-              id="searchDescription"
-              checked={searchDescription}
-              onChange={(e) => {
-                setSearchDescription(e.target.checked);
-              }}
-            />
-            <label htmlFor="searchDescription">Search Descriptions</label>
-          </span>
-          <span className={styles.checkboxcontainer}>
-            <input
-              type="checkbox"
-              id="gridDisplay"
-              checked={displayGrid}
-              onChange={(e) => {
-                setDisplayGrid(e.target.checked);
-              }}
-            />
-            <label htmlFor="gridDisplay">Display in Grid</label>
+        <div className={styles.header}>
+          <div className={styles.optionscontainer}>
+            <span className={styles.searchcontainer}>
+              Project Sekai Updates
+            </span>
+            <span className={styles.searchcontainer}>
+              <label>
+                Filter:
+                <input
+                  type="text"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  onKeyUp={(e) => {
+                    if (
+                      (e.key == "Enter" || e.key == " ") &&
+                      searchRef.current
+                    ) {
+                      searchRef.current.blur();
+                    }
+                  }}
+                  ref={searchRef}
+                />
+              </label>
+            </span>
+            <span className={styles.checkboxcontainer}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={searchDescription}
+                  onChange={(e) => {
+                    setSearchDescription(e.target.checked);
+                  }}
+                />
+                Search Descriptions
+              </label>
+            </span>
+            <span className={styles.checkboxcontainer}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={displayGrid}
+                  onChange={(e) => {
+                    setDisplayGrid(e.target.checked);
+                  }}
+                />
+                Display in Grid
+              </label>
+            </span>
+            <span className={styles.checkboxcontainer}>
+              <label className={styles.noselect}>
+                <input
+                  onChange={() =>
+                    setShowPastUpdates(showPastUpdates ? false : true)
+                  }
+                  type="checkbox"
+                  defaultChecked={showPastUpdates}
+                />
+                Show Past Updates
+              </label>
+            </span>
+          </div>
+          <span className={styles.endItems}>
+            <a
+              className={styles.headerLogoContainer}
+              href="https://github.com/JdavisBro/SekaiUpdates"
+              target="_blank"
+            >
+              {githubLogo}
+            </a>
+            <a className={styles.infoButton} href="#About">
+              <img alt="About Button" src={info} />
+            </a>
           </span>
         </div>
         <div
-          className={displayGrid ? styles.updatecontainergrid : styles.updatecontainer}
+          className={
+            displayGrid ? styles.updatecontainergrid : styles.updatecontainer
+          }
           onWheel={(e) => {
             if (containerRef.current) {
               containerRef.current.scrollLeft += e.deltaY;
@@ -75,7 +134,7 @@ export default function Content(props: Props): React.ReactElement {
             <Update
               key={update.version}
               server={props.server}
-              showPastUpdates={props.showPastUpdates}
+              showPastUpdates={showPastUpdates}
               search={search}
               searchDescription={searchDescription}
               update={update}
